28a3ef881c17bfa449f16383a1302f2eb17473ff
Andrei Korotenko <korotenko.qndroid@gmail.com>
2022-09-05 14:26:07 +0300

    router namespace troubles

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,15 @@
-# Create your urls here
+from django.urls import include, path
+from rest_framework import routers
 
-urlpatterns = []
+from author.views import AuthorViewSet
+
+
+router = routers.DefaultRouter()
+router.register("author", AuthorViewSet)
+
+urlpatterns = [
+    path("", include((router.urls, "author"), namespace="manage-list"))
+]
+
+
+app_name = "authors"
