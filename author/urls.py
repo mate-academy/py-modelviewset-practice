36bcc04474d4e2cfee295862fabc228abb46b1c7
Oleksii Bodnarchuk <olbody12@gmail.com>
2022-10-18 20:43:56 +0300

    refactoring

--- author/urls.py
+++ author/urls.py
@@ -3,15 +3,15 @@ from django.urls import path
 from author.views import AuthorViewSet
 
 author_list = AuthorViewSet.as_view(
-        actions={"get": "list", "post": "create"})
+    actions={"get": "list", "post": "create"})
 
 author_detail = AuthorViewSet.as_view(
-        {
-            "get": "retrieve",
-            "put": "update",
-            "patch": "partial_update",
-            "delete": "destroy",
-        })
+    {
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    })
 
 urlpatterns = [
     path("", author_list, name="manage-list"),
