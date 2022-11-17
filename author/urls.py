a2fee483395b63097e0444a93bba4cfd7fca9800
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-17 18:05:41 +0200

    Fixed tests

--- author/urls.py
+++ author/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from author.views import AuthorViewSet
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet, basename="manage")
+router.register("authors", AuthorViewSet)
 
 urlpatterns = router.urls
 
