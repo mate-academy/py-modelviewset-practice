5b78e25c6b40bc13358fbb4d24ddfa246323d28d
Alexander <alexzubkoff123@gmail.com>
2022-11-17 14:52:42 +0200

    Added migrations

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,21 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets, mixins
+
+from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    class Meta:
+        model = Author
+        fields = "__all__"
+
+
+class AuthorViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,28 @@
 # Create your urls here
+from django.urls import path
 
-urlpatterns = []
+from author.serializers import AuthorViewSet
+
+urlpatterns = [
+    path(
+        "authors/",
+        AuthorViewSet.as_view(
+            {"get": "list", "post": "create"}
+        ),
+        name="manage-list"
+    ),
+    path(
+        "authors/<int:pk>/",
+        AuthorViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="author-detail"
+    ),
+]
+
+app_name = "author"
