a613c426bd75c4ba108bf646820fd20a01b7d2b7
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-29 15:04:15 -0400

    Change urls

--- author/serializers.py
+++ author/serializers.py
@@ -3,21 +3,30 @@ from rest_framework import serializers
 from author.models import Author
 
 
+# class AuthorSerializer(serializers.Serializer):
+#     first_name = serializers.CharField(required=True)
+#     last_name = serializers.CharField(required=True)
+#     pseudonym = serializers.CharField(required=True)
+#     age = serializers.IntegerField(required=True)
+#     retired = serializers.BooleanField(required=True)
+#
+#     def create(self, validated_data):
+#         return Author.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.first_name = validated_data.get(
+#             "first_name", instance.first_name)
+#         instance.last_name = validated_data.get(
+#             "last_name", instance.last_name)
+#         instance.pseudonym = validated_data.get(
+#             "pseudonym", instance.pseudonym)
+#         instance.age = validated_data.get("age", instance.age)
+#         instance.retired = validated_data.get("retired", instance.retired)
+#         instance.save()
+#         return instance
+
+
 class AuthorSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    pseudonym = serializers.CharField(required=True)
-    age = serializers.IntegerField(required=True)
-    retired = serializers.BooleanField(required=True)
-
-    def create(self, validated_data):
-        return Author.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.pseudonym = validated_data.get("pseudonym", instance.pseudonym)
-        instance.age = validated_data.get("age", instance.age)
-        instance.retired = validated_data.get("retired", instance.retired)
-        instance.save()
-        return instance
+    class Meta:
+        model = Author
+        fields = ["first_name", "last_name", "pseudonym", "age", "retired"]
--- author/urls.py
+++ author/urls.py
@@ -7,9 +7,24 @@ from author.views import AuthorViewSet
 router = routers.DefaultRouter()
 router.register("authors", AuthorViewSet)
 
+author_list = AuthorViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"}
+)
+
+author_detail = AuthorViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"}
+)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("authors/", author_list, name="manage-list"),
+    path("authors/<int:pk>/", author_detail, name="author-detail"),
 ]
 
 app_name = "author"
