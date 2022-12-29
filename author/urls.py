db74219b8d2cd76a5d0b5d6a9629bbbfa787bab0
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-29 14:26:00 -0400

    Employed viewsets for model Author

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,15 @@
-# Create your urls here
 
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from author.views import AuthorViewSet
+
+router = routers.DefaultRouter()
+router.register("authors", AuthorViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "author"
