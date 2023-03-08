73136f494c50e69a2a3feb1ab635795493d1ed6c
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-08 19:48:37 +0200

    Fixed bugs

--- author/serializers.py
+++ author/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(max_length=64)
-    last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(max_length=64, required=False)
-    age = serializers.IntegerField()
-    retired = serializers.BooleanField()
+    class Meta:
+        model = Author
+        fields = "__all__"
