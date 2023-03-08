6832822a2415d91f1470224be011010319ee7746
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-08 19:37:09 +0200

    solution1

--- author/serializers.py
+++ author/serializers.py
@@ -2,5 +2,8 @@ from rest_framework import serializers
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    first_name = serializers.CharField(max_length=64)
+    last_name = serializers.CharField(max_length=64)
+    pseudonym = serializers.CharField(max_length=64, required=False)
+    age = serializers.IntegerField()
+    retired = serializers.BooleanField()
