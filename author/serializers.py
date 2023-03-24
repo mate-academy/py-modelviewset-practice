860ea94ee8d75e2b964f5d26d77f5b63509b6018
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-03-24 01:39:00 +0100

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,42 @@
+from typing import Optional
+
 from rest_framework import serializers
 
+from author.models import Author
 
-class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+
+class AuthorSerializer(serializers.Serializer):
+    id_ = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=64, required=True)
+    last_name = serializers.CharField(max_length=64, required=True)
+    pseudonym = serializers.CharField(
+        max_length=64,
+        required=False,
+        allow_blank=True
+    )
+    age = serializers.IntegerField(required=True)
+    retired = serializers.BooleanField(required=True)
+
+    def create(self, validated_data: dict) -> Optional[Author]:
+        return Author.objects.create(**validated_data)
+
+    def update(
+            self,
+            instance: Author,
+            validated_data: Optional[dict]
+    ) -> Author:
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name
+        )
+        instance.pseudonym = validated_data.get(
+            "pseudonym", instance.pseudonym
+        )
+        instance.age = validated_data.get("age", instance.age)
+        instance.retired = validated_data.get("retired", instance.retired)
+
+        instance.save()
+
+        return instance
