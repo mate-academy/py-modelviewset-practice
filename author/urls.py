7a7f150db92c77b7e43733decc1da3e35ce99081
Bohdan Lazaryshyn <lazaryshyn1998@gmail.com>
2023-04-05 15:37:30 +0300

    solution

--- author/urls.py
+++ author/urls.py
@@ -7,14 +7,16 @@ urlpatterns = [
     path("authors/", AuthorViewSet.as_view(actions={
         "get": "list",
         "post": "create"
-        }), name="manage-list"),
+        }
+    ), name="manage-list"),
     path("authors/<int:pk>/", AuthorViewSet.as_view(
         actions={
             "get": "retrieve",
             "put": "update",
             "patch": "partial_update",
             "delete": "destroy",
-        }), name="manage-detail"),
+        }
+    ), name="manage-detail"),
 ]
 
 app_name = "author"
