71b7472c812a87525c07a65be1093c9434aa6078
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-08 15:16:49 +0300

    update urls

--- author/urls.py
+++ author/urls.py
@@ -1,15 +1,10 @@
-from django.urls import path, include
-
 from rest_framework import routers
 
 from author.views import AuthorViewSet
 
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet, basename="manage")
-
-urlpatterns = [
-    path("", include(router.urls))
-]
+router.register("manage", AuthorViewSet, basename="manage")
+urlpatterns = router.urls
 
 app_name = "author"
