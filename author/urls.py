1e7e44cae7cd3eb057953178c371afe56958703f
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-08 15:12:53 +0300

    add basename

--- author/urls.py
+++ author/urls.py
@@ -6,7 +6,7 @@ from author.views import AuthorViewSet
 
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet)
+router.register("authors", AuthorViewSet, basename="manage")
 
 urlpatterns = [
     path("", include(router.urls))
