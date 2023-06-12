026eb67b2c335177e02d3ebb41adf90ed77a2b78
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-06-13 00:16:20 +0300

    Slava Ukraini!

--- author/views.py
+++ author/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 
 from author.models import Author
+from author.serializers import AuthorSerializer
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
-    serializer_class = Author
+    serializer_class = AuthorSerializer
