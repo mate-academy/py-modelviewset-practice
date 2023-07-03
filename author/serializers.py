a6ccb485042b5aa2e79aa3b9ccda446646702153
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-03 21:35:46 +0300

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from author.models import Author
 
 
-class AuthorSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
     pseudonym = serializers.CharField(max_length=64, required=False)
