5b4f06da52ebfb504aa67e3c8f647f17f36d9252
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-07-09 20:23:05 +0300

    Solution_2

--- author/urls.py
+++ author/urls.py
@@ -4,11 +4,20 @@ from rest_framework import routers
 
 from author.views import AuthorViewSet
 
-router = routers.DefaultRouter()
-router.register("author", AuthorViewSet)
+author_list = AuthorViewSet.as_view(actions={
+    "get": "list",
+    "post": "create"
+})
+author_detail = AuthorViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
 
 urlpatterns = [
-    path("", include(router.urls))
-
+    path("authors/", author_list, name="manage-list"),
+    path("authors/<int:pk>/", author_detail, name="manage-detail"),
 ]
+
 app_name = "author"
