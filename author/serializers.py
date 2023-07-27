68f92fcca95b4ebe4d165639e89619d3a8d0a45a
Yaroslav Konoplyov <kannabis252@gmail.com>
2023-07-27 20:12:14 +0300

    added class Meta to serializer where defined model and fields

--- author/serializers.py
+++ author/serializers.py
@@ -4,6 +4,9 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = "__all__"
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
