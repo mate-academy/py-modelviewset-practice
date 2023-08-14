8e0c72ac905cbce1a0671fb0df2da6aa738db3f1
Taras Savchyn <savik1992@gmail.com>
2023-08-14 15:26:47 +0300

    create serializers & AuthorViewSet & urlpatterns

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path, include
 from rest_framework import routers
 
 from author.views import AuthorViewSet
@@ -5,6 +6,7 @@ from author.views import AuthorViewSet
 router = routers.DefaultRouter()
 router.register("authors", AuthorViewSet)
 
-urlpatterns = [] + router.urls
-
+urlpatterns = [
+    path("", include(router.urls,))
+]
 app_name = "author"
