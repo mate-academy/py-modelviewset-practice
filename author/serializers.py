f772479ee3f86a6947b56d9fbf4f03adbaef996c
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-09-24 19:33:01 +0300

    Basename was added

--- author/serializers.py
+++ author/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from author.models import Author
 
 
-class AuthorSerializer(serializers.Serializer):
+class AuthorSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
@@ -10,14 +10,6 @@ class AuthorSerializer(serializers.Serializer):
     age = serializers.IntegerField()
     retired = serializers.BooleanField()
 
-    def create(self, validated_data):
-        return Author.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.pseudonym = validated_data.get("pseudonym", instance.pseudonym)
-        instance.age = validated_data.get("age", instance.age)
-        instance.retired = validated_data.get("retired", instance.retired)
-        instance.save()
-        return instance
+    class Meta:
+        model = Author
+        fields = "__all__"
