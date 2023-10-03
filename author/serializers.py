b63f4c4928adecf62bfacce13475bdef354e525d
Mazur Mariya <mazurinka29@gmail.com>
2023-10-03 18:29:19 +0300

    'Solution'

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,52 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+
+    class Meta:
+        model = Author
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired"
+        )
+        read_only_fields = ("id",)
+
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `Author` instance, given the validated data.
+    #     """
+    #     return Author.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     """
+    #     Update and return an existing `Author` instance,
+    #     given the validated data.
+    #     """
+    #     instance.first_name = validated_data.get(
+    #         "first_name",
+    #         instance.first_name
+    #     )
+    #     instance.last_name = validated_data.get(
+    #         "last_name",
+    #         instance.last_name
+    #     )
+    #     instance.pseudonym = validated_data.get(
+    #         "pseudonym",
+    #         instance.pseudonym
+    #     )
+    #     instance.age = validated_data.get(
+    #         "age",
+    #         instance.age
+    #     )
+    #     instance.retired = validated_data.get(
+    #         "retired",
+    #         instance.retired
+    #     )
+    #     instance.save()
+    #     return instance
--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,74 @@
-# Create your urls here
+# 2. full GenericViewSet & Mixins with routers(better).
+# Можна додавати більше ендпоінтів у перспективі.
+# from django.urls import include, path
+#
+# from .views import AuthorViewSet
+#
+# # 1. імпортую роутери routers
+# from rest_framework import routers
+#
+# # 2. Створюю роутер з routers.DefaultRouter()
+# router = routers.DefaultRouter()
+#
+# # 3. У цей роутер реєструю потрібний набір ендпоінтів,
+# # який буде йти по ключовому слову "buses".
+# # Та вказую який ViewSet за це відповідатиме BusViewSet.
+# router.register("authors", AuthorViewSet)
+# # router.register("trips", TripViewSet)
+# urlpatterns = [
+#     path("", include(router.urls))
+# ]
+#
+# # 3. Вказую назву додатку.
+# app_name = "author"
 
-urlpatterns = []
+# #1. full GenericViewSet & Mixins with routers
+# from django.urls import path
+#
+# from .views import AuthorViewSet
+#
+# # 1. імпортую роутери routers
+# from rest_framework import routers
+#
+# # 2. Створюю роутер з routers.DefaultRouter()
+# router = routers.DefaultRouter()
+#
+# # 3. У цей роутер реєструю потрібний набір ендпоінтів,
+# # який буде йти по ключовому слову "buses".
+# # Та вказую який ViewSet за це відповідатиме BusViewSet.
+# router.register("author", AuthorViewSet)
+#
+# urlpatterns = router.urls
+#
+# # 3. Вказую назву додатку.
+# app_name = "author"
+
+
+# full GenericViewSet & Mixins
+# використовується рідко такий запис. Частіше використовують роутери.
+from django.urls import path
+
+from .views import AuthorViewSet
+
+
+author_list = AuthorViewSet.as_view(actions={
+    "get": "list",
+    "post": "create",
+})
+
+author_detail = AuthorViewSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy",
+})
+
+urlpatterns = [
+    path("authors/", author_list, name="author-list"),
+    path("authors/<pk>/", author_detail, name="author-detailed"),
+    # endpoint, view, name
+]
+
+
+# # 3. Вказую назву додатку.
+app_name = "author"
