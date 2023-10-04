6d142ee416c02b75b048015a8f55ff34001d7cdc
Mazur Mariya <mazurinka29@gmail.com>
2023-10-04 20:28:51 +0300

    'Solution'

--- author/urls.py
+++ author/urls.py
@@ -1,49 +1,11 @@
-# from django.urls import path, include
-# from rest_framework import routers
-#
-#
-# from author.views import AuthorViewSet
-#
-# router = routers.DefaultRouter()
-# router.register("authors", AuthorViewSet, basename="author")
-# # urlpatterns = [
-# #     path("author/", include(router.urls)),
-# # ]
-# urlpatterns = router.urls
-#
-# app_name = "author"
+from rest_framework import routers
 
-#
-# from rest_framework import routers
-#
-# from author.views import AuthorViewSet
-#
-# router = routers.SimpleRouter()
-# router.register("authors", AuthorViewSet, basename="author")
-# include((router.urls, 'app_name'), namespace='instance_name')),
-# urlpatterns = router.urls
-#
-#
-# app_name = "author"
-from django.urls import include, path
 
 from author.views import AuthorViewSet
 
-# 1. імпортую роутери routers
-from rest_framework import routers
-
-# 2. Створюю роутер з routers.DefaultRouter()
 router = routers.DefaultRouter()
+router.register("authors", AuthorViewSet, basename="manage")
 
-# 3. У цей роутер реєструю потрібний набір ендпоінтів,
-# який буде йти по ключовому слову "buses".
-# Та вказую який ViewSet за це відповідатиме BusViewSet.
-router.register("authors", AuthorViewSet)
-# router.register("trips", TripViewSet)
-urlpatterns = [
-    path("", include((router.urls, "author"), namespace="author")),
-]
-#
-# path('api/', include((router.urls, 'app_name'), namespace='instance_name')),
-# 3. Вказую назву додатку.
+urlpatterns = router.urls
+
 app_name = "author"
