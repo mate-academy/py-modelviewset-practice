5c6e5489768d7461ae941b52426162c4c7d0e877
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-12-20 17:53:52 +0200

    Status

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,21 @@
-# Create your urls here
+from django.urls import path
+from author.views import AuthorViewsSet
 
-urlpatterns = []
+author_list = AuthorViewsSet.as_view(actions={
+    "get": "list",
+    "post": "create"
+})
+author_detail = AuthorViewsSet.as_view(actions={
+    "get": "retrieve",
+    "put": "update",
+    "patch": "partial_update",
+    "delete": "destroy"
+})
+
+
+urlpatterns = [
+    path("author/", author_list, name="manage-list"),
+    path("author/<int:pk>/", author_detail, name="manage-detail"),
+]
+
+app_name = "author"
