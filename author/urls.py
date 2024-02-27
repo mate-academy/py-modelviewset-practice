4b8f670a965ae464cd91cba839f3196d6f5a4235
Evheniia Ilchenko <ilchenko.evgeniya96@gmail.com>
2024-02-27 14:48:48 +0200

    fix

--- author/urls.py
+++ author/urls.py
@@ -2,21 +2,22 @@ from django.urls import path
 
 from author.views import AuthorViewSet
 
-app_name = "author"
 
-author_list = AuthorViewSet.as_view(actions={
-    "get": "list",
-    "post": "create",
-})
+author_list = AuthorViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
+author_detail = AuthorViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
 
-author_detail = AuthorViewSet.as_view(actions={
-    "get": "retrieve",
-    "post": "update",
-    "put": "partial_update",
-    "delete": "destroy",
-})
-
+)
 urlpatterns = [
-    path("authors/", author_list, name="author_list"),
-    path("authors/<int:pk>", author_detail, name="author_detail"),
+    path("author/", author_list, name="manage-list"),
+    path("author/<int:pk>/", author_detail, name="manage-detail")
 ]
+
+app_name = "author"
