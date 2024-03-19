514d6b5fbe35e4b5e9518e8b82a53af0aa29fdbb
Alona <alyonkabagritskaya@gmail.com>
2024-03-19 18:39:20 +0200

    fix tests

--- author/urls.py
+++ author/urls.py
@@ -4,8 +4,10 @@ from rest_framework import routers
 from author.views import AuthorViewSet
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet)
+router.register("author", AuthorViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
 ]
+
+app_name = "author"
