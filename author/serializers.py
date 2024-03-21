faa96264c661cd675983687f68f451b6034dcfc2
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-03-21 14:03:43 +0200

    Implemented modelviewset-practice task

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    age = serializers.IntegerField(max_value=100, min_value=1, )
+
+    class Meta:
+        model = Author
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired",
+        ]
